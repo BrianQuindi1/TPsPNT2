1a005ab2b33045f63c7ce4fc49dcc72ba0c2dae8
Brian Isaac Quindi <bquindi@alephee.com>
2025-04-10 21:47:38 -0300

    Parte 2: Métodos de transformación y filtrado

--- tp1.js
+++ tp1.js
@@ -20,3 +20,30 @@ const productos = [
     productos.forEach(pro => {
         console.log("Producto: " + pro.nombre +" - " + "Precio: $" + pro.precio);
     });
+
+    console.log("Ej4");
+
+    let nombres = [];
+
+    productos.map((p) => nombres.push(p.nombre))
+
+    console.log(nombres);
+
+    console.log("Ej5");
+
+    //console.log("find: " + nums.filter( (num) => num > 2));
+    let catRopa = [];
+    productos.filter((p) => p.categoria == "Ropa" ? catRopa.push(p) : "");
+
+    console.log(catRopa);
+
+    console.log("Ej6")
+
+    let precios = [];
+    productos.filter((p) =>p.precio >3000 ? precios.push(p) : "");
+
+    console.log(precios);
+
+    console.log("Ej7");
+    let gorra = productos.find((p) => p.nombre == "Gorra");
+    console.log(gorra);
